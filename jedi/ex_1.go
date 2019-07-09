e223d85b714cb7d251197dca8a5e545695245074
Mikita Kryvitski <solairerove@gmail.com>
2019-07-09 21:14:12 +0300

    feature: complete ex 1 of jedi lvl 5

--- jedi/ex_1.go
+++ jedi/ex_1.go
@@ -4,7 +4,40 @@ import (
 	"fmt"
 )
 
+type person struct {
+	first   string
+	last    string
+	flavors []string
+}
+
 // Ex1 fmt
 func Ex1() {
 	fmt.Println("Ex1")
+
+	p1 := person{
+		first: "mikita",
+		last:  "gulag",
+		flavors: []string{
+			"plombir kak v USSR",
+			"шоколадное удовольствие",
+		},
+	}
+
+	p2 := person{
+		first:   "James",
+		last:    "Bond",
+		flavors: []string{"Hazelnut"},
+	}
+
+	fmt.Println(p1.first, p1.last)
+
+	for _, v := range p1.flavors {
+		fmt.Println(v)
+	}
+
+	fmt.Println(p2.first, p2.last)
+
+	for _, v := range p2.flavors {
+		fmt.Println(v)
+	}
 }
