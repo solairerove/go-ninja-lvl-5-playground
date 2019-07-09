6ec327a0e6b7afc33ca0998b518cd3565023e7aa
Mikita Kryvitski <solairerove@gmail.com>
2019-07-09 21:26:33 +0300

    feature: complete ex2

--- jedi/ex_2.go
+++ jedi/ex_2.go
@@ -6,5 +6,27 @@ import (
 
 // Ex2 fmt
 func Ex2() {
-	fmt.Println("Ex2")
+	p1 := person{
+		first: "mikita",
+		last:  "gulag",
+		flavors: []string{
+			"plombir kak v USSR",
+			"шоколадное удовольствие",
+		},
+	}
+
+	p2 := person{
+		first:   "James",
+		last:    "Bond",
+		flavors: []string{"Hazelnut"},
+	}
+
+	m := map[string]person{
+		p1.last: p1,
+		p2.last: p2,
+	}
+
+	for k, v := range m {
+		fmt.Printf("key -> %v \t value -> %v\n", k, v)
+	}
 }
