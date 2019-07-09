2587bd1401b935636db0be3db1c527a1b6dea5c2
Mikita Kryvitski <solairerove@gmail.com>
2019-07-09 21:39:01 +0300

    feature: complete ex3 and ex4

--- jedi/ex_3.go
+++ jedi/ex_3.go
@@ -4,7 +4,44 @@ import (
 	"fmt"
 )
 
+type vehicle struct {
+	doors rune
+	color string
+}
+
+type truck struct {
+	vehicle
+	forWheel bool
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
 // Ex3 fmt
 func Ex3() {
 	fmt.Println("Ex3")
+
+	t := truck{
+		vehicle: vehicle{
+			doors: 4,
+			color: "black",
+		},
+		forWheel: true,
+	}
+
+	s := sedan{
+		vehicle: vehicle{
+			doors: 3,
+			color: "blue",
+		},
+		luxury: false,
+	}
+
+	fmt.Println(t)
+	fmt.Println(t.doors, t.color, t.forWheel)
+
+	fmt.Println(s)
+	fmt.Println(s.doors, s.color, s.luxury)
 }
--- jedi/ex_4.go
+++ jedi/ex_4.go
@@ -7,4 +7,16 @@ import (
 // Ex4 fmt
 func Ex4() {
 	fmt.Println("Ex4")
+
+	person := struct {
+		first string
+		last  string
+		age   rune
+	}{
+		first: "mikita",
+		last:  "gulag",
+		age:   23,
+	}
+
+	fmt.Println(person)
 }
