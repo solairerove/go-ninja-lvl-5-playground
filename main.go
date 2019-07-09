70c528f4bf236fcca9a21ce7b5a6a604c71f7fe0
Mikita Kryvitski <solairerove@gmail.com>
2019-07-09 20:46:05 +0300

    feature: just setup new repo

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	ex "github.com/solairerove/go-ninja-lvl-5-playground/jedi"
+)
+
+func main() {
+	fmt.Println()
+
+	ex.Ex1()
+
+	fmt.Println()
+
+	ex.Ex2()
+
+	fmt.Println()
+
+	ex.Ex3()
+
+	fmt.Println()
+
+	ex.Ex4()
+}
